d80834c585ab1d282ed33e250828e561dd462ae7
KuanHsien <coson002fy@gmail.com>
2019-06-10 23:07:01 +0800

    Complete main selection list 1. Add DemoType enum for selection list 2. Implement MainSelectionActivity, MainSelectionViewModel, MainSelectionAdapter 3. Add animation. ripple and gradient background for main selection list

---
+++ app/src/main/java/com/kuanhsien/app/sample/android_recyclerview_demo/data/DemoType.kt
@@ -0,0 +1,12 @@
+package com.kuanhsien.app.sample.android_recyclerview_demo.data
+
+import com.kuanhsien.app.sample.android_recyclerview_demo.R
+
+enum class DemoType(val titleRes: Int) {
+    SwipeRefresh(R.string.title_swipe_refresh_activity),
+    ;
+
+    companion object {
+        fun getDemoTypeList(): List<DemoType> = enumValues<DemoType>().toMutableList()
+    }
+}
---
+++ app/src/main/java/com/kuanhsien/app/sample/android_recyclerview_demo/ui/MainSelectionViewModel.kt
@@ -0,0 +1,15 @@
+package com.kuanhsien.app.sample.android_recyclerview_demo.ui
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.kuanhsien.app.sample.android_recyclerview_demo.data.DemoType
+
+class MainSelectionViewModel : ViewModel() {
+
+    private val dataList = DemoType.getDemoTypeList()
+    val demoTypeListLiveData = MutableLiveData<List<DemoType>>()
+
+    fun prepareData() {
+        demoTypeListLiveData.postValue(dataList)
+    }
+}
