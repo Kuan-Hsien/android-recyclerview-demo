e1b6941506068ef1c8416918a84350229ebd29c8
KuanHsien <coson002fy@gmail.com>
2019-07-09 00:25:06 +0800

    Modify prepare data fun in discrete scrollview to use new getItemFrom api in DemoRepository

--- app/src/main/java/com/kuanhsien/app/sample/android_recyclerview_demo/ui/discrete_scrollview/DiscreteScrollViewViewModel.kt
+++ app/src/main/java/com/kuanhsien/app/sample/android_recyclerview_demo/ui/discrete_scrollview/DiscreteScrollViewViewModel.kt
@@ -1,10 +1,10 @@
 package com.kuanhsien.app.sample.android_recyclerview_demo.ui.discrete_scrollview
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.kuanhsien.app.sample.android_recyclerview_demo.data.DemoItem
-import com.kuanhsien.app.sample.android_recyclerview_demo.data.DemoItemRepository
+import com.kuanhsien.app.sample.android_recyclerview_demo.data.*
 
 class DiscreteScrollViewViewModel : ViewModel() {
 
@@ -22,22 +22,37 @@ class DiscreteScrollViewViewModel : ViewModel() {
 
 
     // data
-    private val dataList = mutableListOf<DemoItem>()
-
     // updated data from which position
     private val _updateItemPosLiveData = MutableLiveData<Int>()
     val updateItemPosLiveData: LiveData<Int>
         get() = _updateItemPosLiveData
 
+    private val dataList = mutableListOf<DemoItem>()
     var updateList = mutableListOf<DemoItem>()  // current update list
+    private var nextIndex: Int? = 0     // if no next item, nextIndex would be null
 
 
     // fun
-    fun prepareData(size: Int = 10, fromIndex: Int = dataList.size) {
+    fun prepareData(size: Int = LOAD_DATA_SIZE, index: Int? = nextIndex) {
 
-        updateList = repository.getItemList(size, fromIndex).toMutableList()
-        dataList.addAll(0, updateList)
+        if (index == null) {
+            Log.d("KH_DEMO", "[DiscreteScrollViewViewModel][prepareData] nextIndex == null")
+            return
 
-        _updateItemPosLiveData.postValue(0)
+        } else {
+            val response = repository.getItemFrom(
+                DemoQueryInput(
+                    size = size,
+                    fromIndex = index,
+                    direction = LoadDataDirection.AFTER,
+                    order = LoadDataOrder.DESC
+                )
+            )
+
+            updateList = response.list.toMutableList()
+            dataList.addAll(0, updateList)
+
+            _updateItemPosLiveData.postValue(0)
+        }
     }
 }
